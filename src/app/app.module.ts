5f9f50689fac63943f404131f69811a857abb2eb
Rooney H. Osman <dev@ronios.de>
2022-05-03 21:24:02 +0200

    remove address book form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import { AdressbuchComponent } from './adressbuch/adressbuch.component';
-import { AdressbuchFormComponent } from './adressbuch-form/adressbuch-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdressbuchComponent,
-    AdressbuchFormComponent
   ],
   imports: [
     BrowserModule,
